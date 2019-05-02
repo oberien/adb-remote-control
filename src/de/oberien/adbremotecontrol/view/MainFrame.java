0daea460ff439eebcf9f6208c9a36f1dc557789e
Schlomo Schapiro <schlomo@schapiro.org>
2019-05-02 20:22:07 +0200

    Match window size to device screen size and orientation Fixes #5 Also allow resizing the window

--- src/de/oberien/adbremotecontrol/view/MainFrame.java
+++ src/de/oberien/adbremotecontrol/view/MainFrame.java
@@ -5,6 +5,7 @@ import de.oberien.adbremotecontrol.thread.ScreenshotThread;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
@@ -15,9 +16,29 @@ public class MainFrame extends JFrame {
         add(screenPanel, BorderLayout.CENTER);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(630, 1120);
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        BufferedImage screenshot = device.screenshot();
+        System.out.println("Device screen size " + screenshot.getWidth() + " x " + screenshot.getHeight());
+        int width = screenshot.getWidth(), height = screenshot.getHeight();
+        double aspectRatio = (double) width / (double) height;
+
+        if (width >= screenSize.width || height >= screenSize.height) {
+            if (aspectRatio > 1) {
+                // landscape
+                width = (int) (screenSize.width * 0.8);
+                height = (int) (width / aspectRatio);
+            } else {
+                // portrait
+                height = (int) (screenSize.height * 0.8);
+                width = (int) (height * aspectRatio);
+            }
+        }
+        System.out.println("Initial window size " + width + " x " + height);
+        setSize(width, height);
         setLocationRelativeTo(null);
-        setResizable(false);
+        setResizable(true);
         setVisible(true);
 
         new ScreenshotThread(screenPanel, device).start();
